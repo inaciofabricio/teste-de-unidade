ea736cd33b68be5471cdb4e0faf9aeeafd5b2804
inacio.fabricio.nunes <inacio.fabricio.nunes@gmail.com>
2021-04-12 17:02:22 -0300

    Fim do 1º módulo

---
+++ src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,42 @@
+package br.com.caelum.leilao.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.servico.Avaliador;
+
+public class TesteDoAvaliador {
+
+	@Test
+	public void deveEntenderLancesEmOrdemCrescente() {
+		
+		// parte 1: cenario
+		Usuario joao = new Usuario("João");
+		Usuario maria = new Usuario("Maria");
+		Usuario jose = new Usuario("José");
+		
+		Leilao leilao = new Leilao("Carro usado");
+		
+		leilao.propoe(new Lance(joao, 250));
+		leilao.propoe(new Lance(maria, 350));
+		leilao.propoe(new Lance(jose, 400));
+		
+		// parte 2: ação
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		// parte 3: validação
+		double maiorEsperado = 400;
+		double menorEsperado = 250;
+		
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+		
+		
+	}
+
+}
